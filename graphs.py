4772a28cbbe4031f48ce46c5237411857cb0fb71
AhmedMahmood19 <syed.ahmed.mahmood19@gmail.com>
2022-05-16 20:19:14 +0500

    change thread count

--- graphs.py
+++ graphs.py
@@ -1,10 +1,22 @@
+import pandas as pd
 import numpy as np
-import csv
+# import csv
 
-path = 'merge-normal-log.csv'
-with open(path, 'r') as f:
-    reader = csv.reader(f, delimiter=',')
-    headers = next(reader)
-    data = np.array(list(reader)).astype(float)
+# path = 'merge-normal-log.csv'
+# with open(path, 'r') as f:
+#     reader = csv.reader(f, delimiter=',')
+#     headers = next(reader)
+#     data = np.array(list(reader)).astype(float)
     
-print(headers)
+# print(headers)
+# data = pd.read_csv("merge-normal-log.csv", delimiter=",")
+# print()
+# data.plot(x='data.columns[0]', y='data.columns[1]')
+
+df=pd.DataFrame([1,4,6,8])
+df.hist()
+# import matplotlib.pyplot as plt
+
+# x = np.linspace(0, 20, 100)
+# plt.plot(x, np.sin(x))
+# plt.show()
