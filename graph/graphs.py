a23314c6978ea62d815cc69b8bcfa982bc85e970
AhmedMahmood19 <syed.ahmed.mahmood19@gmail.com>
2022-05-16 22:51:57 +0500

    py for mergesort graphs added

---
+++ graph/graphs.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+df1=pd.read_csv("merge-normal-log.csv")
+df2=pd.read_csv("merge-multithreaded-log.csv")
+ax = df1.plot(x="Array Size",y="Total Time (in s)",label="Single-Threaded",color="green")
+df2.plot(x="Array Size",y="Total Time (in s)",label="Multi-Threaded",color="purple",ax=ax,figsize=(15,10),fontsize=14,title="Merge Sort")
+plt.ylabel("Time (In Seconds)", fontsize=18)
+plt.xlabel("Number of Elements in Array", fontsize=18)
+ax.title.set_fontsize(26)       #fontsize of "Merge Sort"
+ax.legend(loc=2, prop={'size': 18}) #fontsize of legend "Single threaded etc"
+ax.ticklabel_format(useMathText=True)   #replaces 1e9 with x10^9 
+plt.ylim(0, 200)    #sets limit of y axis
+plt.xlim(0, 1.1e9)  #sets limit of x axis
+plt.show()
